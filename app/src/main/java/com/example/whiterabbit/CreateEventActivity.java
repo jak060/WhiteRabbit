a4ddfef18d11121fd3caa242bfd45f4633c619a3
Esther Jun Kim <esther.jun@gmail.com>
2015-07-31 14:27:29 -0700

    Remove onCreateOptionsMenu()
    No action bar items are needed for the Create Event view.

--- app/src/main/java/com/example/whiterabbit/CreateEventActivity.java
+++ app/src/main/java/com/example/whiterabbit/CreateEventActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -232,13 +234,6 @@ public class CreateEventActivity extends AppCompatActivity {
                 showInvitees.setText(temp);
             }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
