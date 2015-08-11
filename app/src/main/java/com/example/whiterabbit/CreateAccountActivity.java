b011bf148c4edbdc6d899482f02227b3ee20d7b6
Esther Jun Kim <esther.jun@gmail.com>
2015-08-11 16:51:36 -0700

    Remove menu inflation methods for activities where there shouldn't be action bar items

--- app/src/main/java/com/example/whiterabbit/CreateAccountActivity.java
+++ app/src/main/java/com/example/whiterabbit/CreateAccountActivity.java
@@ -232,29 +232,6 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     // A method that checks the length of the passed string and set whether it's empty or not.
     // And then it sets the corresponding text message
     public void checkLength(String str, TextView textView, boolean isEmpty) {
